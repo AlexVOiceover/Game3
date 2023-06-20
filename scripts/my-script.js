8f543fef23145f8114db7c5f076d7460e2119ad2
Alexander <alex@alexvoiceover.com>
2023-06-20 21:54:47 +0100

    Added title to landing page

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,30 +1,7 @@
-
-/*const playAudio = (src, callback) => {
-  const audio = new Audio(src);
-  audio.play();
-  audio.addEventListener("ended", callback);
-};
-*/
-
-/*
 // Buttons for the landing page
 document.getElementById("spyButton").addEventListener("click", () =>  window.location.href = "./spy.html");
 document.getElementById("decoderButton").addEventListener("click", () =>  window.location.href = "./decoder.html");
-*/
 
-/*
-window.addEventListener("devicemotion", handleMotion, true);
-
-function handleMotion(event) {
-  const accelerationX = event.acceleration.x;
-  const accelerationY = event.acceleration.y;
-  const accelerationZ = event.acceleration.z;
-
-  const motionData = document.getElementById("motion-data");
-  if (motionData){
-    motionData.innerHTML = `Acceleration X: ${accelerationX?.toFixed(2) || 'N/A'}, Acceleration Y: ${accelerationY?.toFixed(2) || 'N/A'}, Acceleration Z: ${accelerationZ?.toFixed(2) || 'N/A'}`;
-  }
-} */
 const playAudio = (src, callback) => {
   const audio = new Audio(src);
   audio.play();
