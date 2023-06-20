c95515e149b049130c4d1c232bc1d955fd762e3a
alex <alex@alexvoiceover.com>
2023-06-20 19:16:25 +0100

    Test with acceleration

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -6,6 +6,7 @@
 };
 */
 
+// Buttons for the landing page
 document.getElementById("spyButton").addEventListener("click", () =>  window.location.href = "./spy.html");
 document.getElementById("decoderButton").addEventListener("click", () =>  window.location.href = "./decoder.html");
 
@@ -13,9 +14,9 @@ document.getElementById("decoderButton").addEventListener("click", () =>  window
 window.addEventListener("devicemotion", handleMotion, true);
 
 function handleMotion(event) {
-  const accelerationX = event.accelerationIncludingGravity.x;
-  const accelerationY = event.accelerationIncludingGravity.y;
-  const accelerationZ = event.accelerationIncludingGravity.z;
+  const accelerationX = event.acceleration.x;
+  const accelerationY = event.acceleration.y;
+  const accelerationZ = event.acceleration.z;
 
   const motionData = document.getElementById("motion-data");
   motionData.innerHTML = `Acceleration X: ${accelerationX.toFixed(2)}, Acceleration Y: ${accelerationY.toFixed(2)}, Acceleration Z: ${accelerationZ.toFixed(2)}`;
