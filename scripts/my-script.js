0c635954b691251872958658925f6f5a37059718
alex <alex@alexvoiceover.com>
2023-06-20 19:11:27 +0100

    Test with motion-date

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,10 +1,10 @@
 
-const playAudio = (src, callback) => {
+/*const playAudio = (src, callback) => {
   const audio = new Audio(src);
   audio.play();
   audio.addEventListener("ended", callback);
 };
-
+*/
 
 document.getElementById("spyButton").addEventListener("click", () =>  window.location.href = "./spy.html");
 document.getElementById("decoderButton").addEventListener("click", () =>  window.location.href = "./decoder.html");
