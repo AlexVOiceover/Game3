d316e4580aa6a76edb836131df73d3542e9087a5
Alexander <alex@alexvoiceover.com>
2023-06-21 13:15:43 +0100

    Added morse code player

--- scripts/spy_scripts.js
+++ scripts/spy_scripts.js
@@ -25,3 +25,57 @@ function handleOrientation(event) {
     motionData.innerHTML = `Roll: ${roll?.toFixed(2) || 'N/A'}`;
   }
 }
+
+//Function to play a tone given a frequency and duration
+function playTone(frequency, duration) {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const oscillator = audioContext.createOscillator();
+  const gainNode = audioContext.createGain();
+
+  oscillator.type = 'sine';
+  oscillator.frequency.value = frequency;
+  gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+  gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + 0.01);
+  gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime + duration - 0.01);
+
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+  oscillator.start(audioContext.currentTime);
+  oscillator.stop(audioContext.currentTime + duration);
+}
+
+function playMorseCode(char) {
+  const morseCode = {
+      'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',
+      'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---',
+      'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',
+      'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
+      'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--',
+      'Z': '--..',  '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+      '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'
+  };
+
+  const multiplier = 1;
+  const dotDuration = 0.1 * multiplier; // 100ms
+  const dashDuration = 0.3 * multiplier; // 300ms
+  const gapDuration = 0.3 * multiplier; // 100ms
+  const frequency = 2000; // 1000Hz
+
+  const code = morseCode[char.toUpperCase()];
+  let currentTime = 0;
+
+  for (let i = 0; i < code.length; i++) {
+      setTimeout(() => {
+          playTone(frequency, code[i] === '.' ? dotDuration : dashDuration);
+      }, currentTime * 1000);
+
+      currentTime += (code[i] === '.' ? dotDuration : dashDuration) + gapDuration;
+  }
+}
+
+document.getElementById("playMorseCode").addEventListener("click", () => {
+  const inputChar = document.getElementById("inputChar").value;
+  if (inputChar.length === 1) {
+      playMorseCode(inputChar);
+  }
+});
