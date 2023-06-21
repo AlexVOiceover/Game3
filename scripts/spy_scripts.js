117ffcfb93a990da0263e13f545aeca49768b4f0
Alexander <alex@alexvoiceover.com>
2023-06-21 14:54:19 +0100

    Added textbox to show the value of the slider

--- scripts/spy_scripts.js
+++ scripts/spy_scripts.js
@@ -94,4 +94,6 @@ document.getElementById("playMorseCode").addEventListener("click", () => {
 document.getElementById("musicSpeedSlider").addEventListener("input", () => {
   const sliderValue = document.getElementById("musicSpeedSlider").value;
   backgroundMusic.playbackRate = sliderValue;
+  document.getElementById("sliderValueTextbox").value = sliderValue; // Update the textbox value
 });
+
