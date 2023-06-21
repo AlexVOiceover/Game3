77a8989566ad3f4c7dbeb5faa59cad125ad818a4
Alexander <alex@alexvoiceover.com>
2023-06-21 13:18:37 +0100

    Morse beeps wont play on phone

--- scripts/spy_scripts.js
+++ scripts/spy_scripts.js
@@ -59,7 +59,7 @@ function playMorseCode(char) {
   const dotDuration = 0.1 * multiplier; // 100ms
   const dashDuration = 0.3 * multiplier; // 300ms
   const gapDuration = 0.3 * multiplier; // 100ms
-  const frequency = 2000; // 1000Hz
+  const frequency = 1000; // 1000Hz
 
   const code = morseCode[char.toUpperCase()];
   let currentTime = 0;
