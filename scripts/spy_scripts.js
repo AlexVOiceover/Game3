6050dc994b6aaa7a5c4b50fce077809921466e7a
Alexander <alex@alexvoiceover.com>
2023-06-22 17:00:35 +0100

    Removed deviation for testing

--- scripts/spy_scripts.js
+++ scripts/spy_scripts.js
@@ -87,7 +87,7 @@ const ctx = canvas.getContext("2d");
 function drawDot(x, y) {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
   ctx.beginPath();
-  ctx.arc(x+xdeviation, y+ydeviation, 5, 0, 2 * Math.PI); // Draw a dot with a radius of 5
+  ctx.arc(x, y, 5, 0, 2 * Math.PI); // Draw a dot with a radius of 5
   ctx.fillStyle = "white";
   ctx.fill();
 }
