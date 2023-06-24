5eb5e4d37689bae6040954d254b2041790c9a721
Alexander <alex@alexvoiceover.com>
2023-06-24 19:16:54 +0100

    Add button to allow play sounds

--- scripts/decoder_scripts.js
+++ scripts/decoder_scripts.js
@@ -1,21 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const morseButton = document.getElementById("morseButton");
     const morseTextbox = document.getElementById("morseTextbox");
+    const enableAudioButton = document.getElementById("enableAudioButton");
   
     let pressStartTime;
     let pressDuration;
     let morseInput = "";
     let audioContext = new (window.AudioContext || window.webkitAudioContext)();
     let oscillator;
+    let audioEnabled = false;
+  
+    enableAudioButton.addEventListener("click", () => {
+      audioEnabled = true;
+      enableAudioButton.disabled = true;
+    });
   
     morseButton.addEventListener("pointerdown", () => {
       pressStartTime = new Date();
   
-      oscillator = audioContext.createOscillator();
-      oscillator.frequency.value = 750; // Set the frequency of the tone
-      oscillator.type = "sine"; // Set the type of wave
-      oscillator.connect(audioContext.destination);
-      oscillator.start();
+      if (audioEnabled) {
+        oscillator = audioContext.createOscillator();
+        oscillator.frequency.value = 750; // Set the frequency of the tone
+        oscillator.type = "sine"; // Set the type of wave
+        oscillator.connect(audioContext.destination);
+        oscillator.start();
+      }
     });
   
     morseButton.addEventListener("pointerup", () => {
@@ -23,8 +32,10 @@ document.addEventListener("DOMContentLoaded", () => {
       const morseChar = pressDuration < 300 ? "." : "-"; // Threshold of 300ms to differentiate between dot and dash
       morseInput += morseChar;
   
-      // Stop playing the tone
-      oscillator.stop();
+      if (audioEnabled) {
+        // Stop playing the tone
+        oscillator.stop();
+      }
   
       // Update the textbox
       morseTextbox.value = morseInput;
