4f522f269829a3f51d5122b7a0abf6c23847ca6f
Alexander <alex@alexvoiceover.com>
2023-06-30 22:16:34 +0100

    Fixed selectaudio play

--- scripts/index_scripts.js
+++ scripts/index_scripts.js
@@ -45,8 +45,8 @@ function handleTouch(event) {
     proceedButton.disabled = false;
     proceedButton.classList.remove("disabled");
 
-    const selectAudio = new Audio("./audios/select.ogg");
-    Audio.play();
+    const selectAudio = new audio("./audios/select.ogg");
+    audio.play();
 
     if (touchX < imgWidth / 2) {
       imgDiv.classList.remove("right");
