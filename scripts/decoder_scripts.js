e80891694f195661605a9f5cb6e55989549f7ca6
Alexander <alex@alexvoiceover.com>
2023-07-10 19:11:14 +0100

    delete last signal will reactivarte decoder

--- scripts/decoder_scripts.js
+++ scripts/decoder_scripts.js
@@ -62,7 +62,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let translationTimeout;
 
     const translateMorseCode = () => {
-     // console.log("Dentro translateMorseCode" +morseInput);
       morseInput = morseTextbox.value.trim();
       const character = Object.keys(morseCode).find((key) => morseCode[key] === morseInput);
   
@@ -95,9 +94,13 @@ document.addEventListener("DOMContentLoaded", () => {
   //This also needs to be modified if change to array
   deleteLastSignal.addEventListener("pointerdown", () => {
     // Check if arrayMorseTextbox.value is not empty
+    
     if (arrayMorseTextbox.value.length > 0) {
       // Remove the last character of the string
       arrayMorseTextbox.value = arrayMorseTextbox.value.slice(0, -1);
+      //Activate device and siwtch to carry one after deleting
+      device.disabled = false;
+      enableAudioSwitch.checked = true;
     }
   });
   
